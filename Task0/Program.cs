360511f199233ceb8913b001ef093f0f2755c4ee
Andrey_Alekseev <aragirn1402@gmail.com>
2022-08-07 13:57:36 +0300

    Выполнена 0 задача

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на выход принимает число и выдаёт его квадрат
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.WriteLine($"Квадрат числа {num} равен {square}");
