938b12a5fbb53c1d4f8194043ddb358131923730
Andrey_Alekseev <aragirn1402@gmail.com>
2022-08-13 10:50:24 +0300

    Выполнено д/з вторго семинара

---
+++ Task15/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 5 & num > 0) System.Console.WriteLine($"{num} являтся будним днём");
+else if (num <= 7 & num > 5 & num < 8) System.Console.WriteLine($"{num} являтся выходным днём");
+else System.Console.WriteLine("Чесло не входит в диапозон от 1 до 7");
