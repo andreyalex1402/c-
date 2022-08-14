fd20ded953534c0cf9ae3e840cbc1986f94f1f06
Andrey_Alekseev <aragirn1402@gmail.com>
2022-08-14 11:37:00 +0300

    выполнено (д/з) третьго семинара

---
+++ Task19/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string numString = num.ToString(); 
+string rev = string.Concat(numString.Reverse());
+Console.WriteLine(numString == rev ? "да" : "нет");
