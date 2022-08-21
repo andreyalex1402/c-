eeb3dcbf7ed88a0846a502fad3465d9029770126
Andrey_Alekseev <aragirn1402@gmail.com>
2022-08-21 15:51:33 +0300

    выполнено д/з 5 семинара

---
+++ Task36/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+int[] array = new int[4];
+Random myRandom = new Random();
+int a = 0;
+for (int i = 0; i < 4; i++) 
+    { 
+        array[i] = myRandom.Next(-99, 100);
+        Console.Write($"{array[i]} ");
+        if (i !=0 & i%2 !=0)
+        {
+            a = a + array[i]; 
+        }
+    }    
+Console.WriteLine($"->{a}");
