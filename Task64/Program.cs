ec559903c9e26e744d99b64b3f8e8e39df7919cf
Andrey_Alekseev <aragirn1402@gmail.com>
2022-09-10 18:36:01 +0300

    дз9

---
+++ Task64/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введи N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = N ; i >=1 ; i--)
+{
+    if (i>1)
+    Console.Write($" {i},");
+    else
+    Console.Write($" {i}");
+}
