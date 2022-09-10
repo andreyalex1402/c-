bc942b3988fcfa397f060ca22763e920fa547f20
Andrey_Alekseev <aragirn1402@gmail.com>
2022-09-10 09:48:53 +0300

    дз 9

---
+++ Task66/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введи N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write($"N={N}, M={M}");
+int a = 0;
+if (M>N)
+for (int i = N; i <= M; i++)
+    a = a + i;
+else
+    for (int i = M; i <= N; i++)
+        a = a + i;
+Console.Write($" -> {a}");        
